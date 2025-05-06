de59fc1a280098eb3be6cac285e084ceaf4a7448
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-05-06 18:03:53 +0530

    feat : model added for auther follower/following system

--- Tatetailor/Mystory/models.py
+++ Tatetailor/Mystory/models.py
@@ -88,3 +88,11 @@ class CollaborationInvite(models.Model):
         """Rejects the invite."""
         self.status = "rejected"
         self.save()
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
