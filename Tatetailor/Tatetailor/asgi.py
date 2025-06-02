6de80c3e86b427e3217f08de263c723895c022a9
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-02 19:25:43 +0530

    feat asgi.py for dockeraize for  chat

--- Tatetailor/Tatetailor/asgi.py
+++ Tatetailor/Tatetailor/asgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
+# import os
 
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tatetailor.settings')
+
+# application = get_asgi_application()
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import messaging.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tatetailor.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            messaging.routing.websocket_urlpatterns
+        )
+    ),
+})
