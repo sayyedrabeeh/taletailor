103e175f2e44fbb2bc7739a9160abc7c3ef31478
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-02 19:41:24 +0530

    feat models in  messaging app for chat

--- Tatetailor/messaging/models.py
+++ Tatetailor/messaging/models.py
@@ -1,3 +1,13 @@
+ 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    participants = models.ManyToManyField(User)
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
