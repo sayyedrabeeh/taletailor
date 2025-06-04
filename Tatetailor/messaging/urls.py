2a649e1370afe59368bcce65bbe04f2717dbb49c
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-04 13:53:20 +0530

    fix urls  send message  on views   for  chaatroom

--- Tatetailor/messaging/urls.py
+++ Tatetailor/messaging/urls.py
@@ -1,13 +1,15 @@
  
 from django.contrib import admin
 from django.urls import path 
-from .views import chat_room,chat
+from .views import chat_room,chat,send_message
 
 app_name = 'messaging' 
 
 urlpatterns = [
-   path('chat/<str:room_name>/', chat_room, name='chat_room'),
+   
    path('chat/', chat, name='chat'),
+    path('chat/<str:room_name>/', chat_room, name='chat_room'),
+    path('chat/<str:room_name>/send/', send_message, name='send_message'),
    
 ]
 
