38c436a407be9d25cacdccbbb19526d5a6249562
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-13 19:04:01 +0530

    fix add consumer for display realtime notification

--- Tatetailor/messaging/consumers.py
+++ Tatetailor/messaging/consumers.py
@@ -49,3 +49,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "message": message,
             "username": username,
         }))
+
+ 
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.username = self.scope["url_route"]["kwargs"]["username"]
+        self.group_name = f"notify_{self.username}"
+
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        # You can handle commands here if needed
+        pass
+
+    async def send_notification(self, event):
+        await self.send(text_data=json.dumps(event["data"]))
