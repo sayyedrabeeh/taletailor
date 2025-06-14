e6351f42e9a19aa12951f481df12d654a4ff29cc
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-14 21:59:58 +0530

    fix online and last seen in chat

--- Tatetailor/messaging/apps.py
+++ Tatetailor/messaging/apps.py
@@ -5,10 +5,5 @@ class MessagingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "messaging"
 
-from django.apps import AppConfig
-
-class MessagingConfig(AppConfig):
-    name = 'messaging'
-
     def ready(self):
         import messaging.signals
