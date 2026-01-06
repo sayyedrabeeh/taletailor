1a27a371ad15a24711b1194e402e027f2b2d6827
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2026-01-06 23:48:36 +0530

    add some more  theame

--- Tatetailor/messaging/apps.py
+++ Tatetailor/messaging/apps.py
@@ -4,4 +4,7 @@ from django.apps import AppConfig
 class MessagingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "messaging"
- 
+
+    def ready(self):
+        # import messaging.signals # Disabled to prevent duplicate profile creation
+        pass
